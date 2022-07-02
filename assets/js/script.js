7d867cd13ef06b8f1bbc99abc9fce6068a8cabd7
zzzorigtbaatar <zzzorigtbaatar@gmail.com>
2022-07-01 21:05:50 -0700

    add timeblocks to page

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+//Add current day to header
+var currentDay = $("#currentDay");
+currentDay.text(moment().format("dddd, MMMM Do"));
+
+
+//Add timeblocks to page
+function displayTimeblocks(){
+    for(var i = 6; i <=22; i++){
+        var container = $(".container");
+        var groupDiv = $("<div>").addClass("row time-block");
+        var timeDiv = $("<div>").text(timeString(i)).addClass("hour col-1");
+        var noteDiv = $("<input>").addClass("justify-content-center col-10 " + timeColor(i))
+        var saveDiv = $("<div>").text("💾").addClass("saveBtn col-1");
+
+        groupDiv.append(timeDiv, noteDiv, saveDiv);
+        container.append(groupDiv);
+    }
+}
+
+//Converts 24 hour format into AM and PM format
+function timeString(int){ 
+    if(int < 12){
+        return (int+"AM");
+    }
+    if(int == 12){
+        return (int+"PM");
+    } else {
+        return ((int-12)+"PM");
+    }
+}
+
+//Changes color of timeblock depending on state
+function timeColor(time){
+    if(time == moment().format("H")){
+        return "present";
+    } else if(time < moment().format("H")){
+        return "past";
+    } else {
+        return "future";
+    }
+}
+
+//Calls function to put up timeblocks
+displayTimeblocks();
